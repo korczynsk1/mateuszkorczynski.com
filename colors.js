c99849d3dfa564d9440ea31f4d198c253d3f4ad4
spaghetticodebonara <spaghetticodebonara@gmail.com>
2019-05-06 00:47:34 +0200

    project on Gatsby

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#E0E0E0',
+
+  primary: '#00A6D7',
+  primaryLight: '#00A6D7',
+  primaryDark: '#00A6D7',
+
+  secondary: '#D3D3D3',
+  secondaryLight: '#EBEBEB',
+  secondaryDark: '#3E3E3D',
+};
---
+++ manifest-config.js
@@ -0,0 +1,11 @@
+const colors = require('./colors');
+
+module.exports = {
+  name: 'Mateusz Korczynski',
+  short_name: 'Mateusz Korczynski',
+  start_url: '/',
+  background_color: colors.background,
+  theme_color: colors.primary,
+  display: 'minimal-ui',
+  icon: 'src/components/Logo/portfolio.png',
+};
